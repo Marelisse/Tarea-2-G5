3fdb3f3d00da387a92265d4a9e7dd16c478c550c
Arcisternas <arcisternas2023@udec.cl>
2024-05-12 17:12:34 -0400

    feat: SubClases ReunionPresencial y ReunionVirtual
    -Se crean las subclases de Reunion "ReunionPresencial" y "ReunionVirtual", aun sin implementación.

---
+++ demo/src/main/java/tarea2/ReunionPresencial.java
@@ -0,0 +1,15 @@
+package tarea2;
+import java.util.List;
+
+public class ReunionPresencial {
+    private String sala;
+    public ReunionPresencial(){}
+    public List<Empleado> obtenerAsistencias(){return null;};
+    public List<Empleado> obtenerAusencias(){return null;};
+    public List<Empleado> obtenerRetrasos(){return null;};
+    public int obtenerTotalAsistencia(){return 0;};
+    public float obtenerPorcentajeAsistencia(){return 0;};
+    public float calcularTiempoReal(){return 0;};
+    public void iniciar(){};
+    public void finalizar(){}; 
+}
---
+++ demo/src/main/java/tarea2/ReunionVirtual.java
@@ -0,0 +1,15 @@
+package tarea2;
+import java.util.List;
+
+class ReunionVirtual extends Reunion {
+    private String enlace;
+    public ReunionVirtual(){}
+    public List<Empleado> obtenerAsistencias(){return null;};
+    public List<Empleado> obtenerAusencias(){return null;};
+    public List<Empleado> obtenerRetrasos(){return null;};
+    public int obtenerTotalAsistencia(){return 0;};
+    public float obtenerPorcentajeAsistencia(){return 0;};
+    public float calcularTiempoReal(){return 0;};
+    public void iniciar(){};
+    public void finalizar(){}; 
+}
