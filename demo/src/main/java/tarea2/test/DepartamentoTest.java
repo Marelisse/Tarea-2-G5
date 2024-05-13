a2c466cf5fe6a7f7dab6454fa1b7be40e37b53ec
Arcisternas <arcisternas2023@udec.cl>
2024-05-13 10:08:21 -0400

    feat: Tests base
    -Se añaden las Dependencias de JUnit al proyecto de Maven.
    -Se crea la Carpeta test para gestionar estos mismos.
    -Se crea la Estructura de los test para Departamento y Reunion, aun sin implementación

---
+++ demo/src/main/java/tarea2/test/DepartamentoTest.java
@@ -0,0 +1,27 @@
+package tarea2.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DepartamentoTest {
+    @BeforeEach
+    void setUp() {
+    }
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    @DisplayName("Obtener cantidad de empleados")
+    public void testbtenerCantidadEmpleados() {
+    }
+    @Test
+    @DisplayName("Agregar empleado")
+    public void testagregarEmpleado() {
+    }
+    @Test
+    @DisplayName("Invitar")
+    public void testinvitar() {
+    }
+}
---
+++ demo/src/main/java/tarea2/test/ReunionTest.java
@@ -0,0 +1,47 @@
+package tarea2.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReunionTest {
+    @BeforeEach
+    void setUp() {
+    }
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    @DisplayName("Obtener asistencias")
+    public void testObtenerAsistencias() {
+    }
+    @Test
+    @DisplayName("Obtener ausencias")
+    public void testObtenerAusencias() {
+    }
+    @Test
+    @DisplayName("Obtener retrasos")
+    public void testObtenerRetrasos() {
+    }
+    @Test
+    @DisplayName("Obtener total asistencia")
+    public void testObtenerTotalAsistencia() {
+    }
+    @Test
+    @DisplayName("Obtener porcentaje asistencia")
+    public void testObtenerPorcentajeAsistencia() {
+    }
+    @Test
+    @DisplayName("Calcular tiempo real")
+    public void testCalcularTiempoReal() {
+    }
+    @Test
+    @DisplayName("Iniciar")
+    public void testIniciar() {
+    }
+    @Test
+    @DisplayName("Finalizar")
+    public void testFinalizar() {
+    }
+}
