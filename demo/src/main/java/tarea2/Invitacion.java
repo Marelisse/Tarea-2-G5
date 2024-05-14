ee89341f224c5eeaeeedb33d2b70440cadc1fa98
Arcisternas <arcisternas2023@udec.cl>
2024-05-14 17:34:41 -0400

    feat: Clase Invitacion
    -Se define más especificamente la clase Invitación

--- demo/src/main/java/tarea2/Invitacion.java
+++ demo/src/main/java/tarea2/Invitacion.java
@@ -3,4 +3,15 @@ import java.time.Instant;
 
 public class Invitacion {
     private Instant hora;
+    private Invitable invitado;
+    public Invitacion(Invitable invitado, Instant hora){
+        this.invitado = invitado;
+        this.hora = hora;
+    }
+    public Invitable getInvitado(){
+        return invitado;
+    }
+    public Instant getHora(){
+        return hora;
+    }
 }
