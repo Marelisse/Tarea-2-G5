de7a66cb6fdc75c2b7ba32f0227709f7e482d1af
María Jesús <marolivares2023@udec.cl>
2024-05-15 17:59:56 -0400

    fix: Reunion
    Se realizan cambios a beneficio de sus subclases

--- demo/src/main/java/tarea2/Reunion.java
+++ demo/src/main/java/tarea2/Reunion.java
@@ -2,18 +2,15 @@ package tarea2;
 import java.sql.Date;
 import java.time.Instant;
 import java.util.List;
-import java.util.ArrayList;
 
 public abstract class Reunion {
-    private Date fecha;
-    private Instant horaPrevista;
-    private Instant horaInicio;
-    private Instant horaFin;
-    protected List<String> notas;
+    protected Date fecha;
+    protected Instant horaPrevista;
+    protected Instant horaInicio;
+    protected Instant horaFin;
+    protected List<Nota> notas;
 
-    public Reunion(){
-        notas = new ArrayList<String>();
-    }
+    public Reunion(){}
     public abstract List<Empleado> obtenerAsistencias();
     public abstract List<Empleado> obtenerAusencias();
     public abstract List<Empleado> obtenerRetrasos();
@@ -23,5 +20,5 @@ public abstract class Reunion {
     public abstract void iniciar();
     public abstract void finalizar();
     public abstract void crearNota(String contenido); 
-    public abstract List<String> obtenerNotas();
+    public abstract List<Nota> obtenerNotas();
 }
